efd1ef2849ff608ebf96af179a89813869d20675
Padma Patil <padma@email.com>
2023-12-12 17:21:01 -0800

    add UI logic to js

--- scripts.js
+++ scripts.js
@@ -9,4 +9,32 @@ function PizzaOrder(size, topping) {
       medium: { name: "Medium", price: 25 },
       large: { name: "Large", price: 30 }
     };
-}
+
+const selectedSize = sizeOptions[this.size];
+const totalPrice = selectedSize.price;
+const pizzaDescription = `${selectedSize.name} pizza with ${this.topping}`;
+
+return `Total Cost: $${totalPrice}. ${pizzaDescription}.`;
+};
+
+// UI Logic
+function handlePizzaFormSubmit(event) {
+    event.preventDefault();
+  
+    const selectedTopping = document.querySelector('input[name="topping"]:checked');
+    const selectedSize = document.querySelector('select[name="size"]');
+  
+    if (selectedTopping && selectedSize) {
+      const topping = selectedTopping.value;
+      const size = selectedSize.value;
+      const pizzaOrder = new PizzaOrder(size, topping);
+  
+      const totalCostElement = document.getElementById("totalCost");
+      totalCostElement.textContent = pizzaOrder.calculateTotalCost();
+    }
+  }
+  
+  window.addEventListener("load", function() {
+    const pizzaForm = document.getElementById("pizzaForm");
+    pizzaForm.addEventListener("submit", handlePizzaFormSubmit);
+  });
